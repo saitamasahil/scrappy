d90d90d120fc88177058c69f951f2639c11240bc
Gabriel Vale <gabrielfvale@users.noreply.github.com>
2024-11-06 19:34:06 -0300

    feat: log cleanup

--- lib/log.lua
+++ lib/log.lua
@@ -1,13 +1,28 @@
 local log = {}
 
+local nativefs = require("lib.nativefs")
 local socket = require("socket")
+
 local log_backend = love.thread.newThread("lib/log_backend.lua")
 local log_channel = love.thread.getChannel("log")
+local max_logs = 10
 
 local start_time
 
+local function cleanup_logs()
+  local files = nativefs.getDirectoryItems("logs")
+  if #files > max_logs then
+    for i = 1, #files - max_logs do
+      if files[i]:sub(-4) == ".log" then
+        nativefs.remove("logs/" .. files[i])
+      end
+    end
+  end
+end
+
 function log.start()
   start_time = socket.gettime()
+  cleanup_logs()
   log_backend:start(start_time)
   log_channel:push("start")
 end
