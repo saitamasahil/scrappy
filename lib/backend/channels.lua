c055289b46f43c100d82a27fa040cf5d0d422797
Gabriel Vale <gabrielfvale@users.noreply.github.com>
2025-02-03 20:29:49 -0300

    feat: file watcher

--- lib/backend/channels.lua
+++ lib/backend/channels.lua
@@ -3,6 +3,8 @@ local channels = {
   SKYSCRAPER_INPUT  = love.thread.getChannel("skyscraper_command"),
   SKYSCRAPER_OUTPUT = love.thread.getChannel("skyscraper_output"),
   TASK_OUTPUT       = love.thread.getChannel("task_output"),
+  WATCHER_INPUT     = love.thread.getChannel("watcher_input"),
+  WATCHER_OUTPUT    = love.thread.getChannel("watcher_output"),
 }
 
 return channels
