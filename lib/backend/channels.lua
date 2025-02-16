8cd981186ac5288fb4d7e183f84d21a8ef3e7725
Gabriel Vale <gabrielfvale@users.noreply.github.com>
2025-02-16 18:59:53 -0300

    chore: remove file watcher

--- lib/backend/channels.lua
+++ lib/backend/channels.lua
@@ -6,8 +6,6 @@ local channels = {
   SKYSCRAPER_GAME_QUEUE = love.thread.getChannel("skyscraper_midleware"),
   SKYSCRAPER_GEN_OUTPUT = love.thread.getChannel("skyscraper_generate_output"),
   TASK_OUTPUT           = love.thread.getChannel("task_output"),
-  WATCHER_INPUT         = love.thread.getChannel("watcher_input"),
-  WATCHER_OUTPUT        = love.thread.getChannel("watcher_output"),
 }
 
 return channels
